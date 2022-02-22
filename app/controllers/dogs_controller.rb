976d6dfe33b01016cf4c313880dac8fd15ebb028
Ludwig Kirschning <l.kirschning@yahoo.de>
2022-02-22 11:52:41 +0100

    created index page, new page still in progress

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,36 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    set_dog
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+  end
+
+  def destroy
+    @dog = set_dog
+    @dog.delete
+  end
+
+  private
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:price, :name, :gender, :breed, :age, :size, :description)
+  end
+
+
+
 end
