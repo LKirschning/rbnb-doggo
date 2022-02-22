10f27fc4e6f70b2841ac729326c16c0bc07b026c
Andres Benegas <andres.benegas@ymail.com>
2022-02-22 12:08:57 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up Database…"
+Dog.destroy_all
+puts "Database is clean"
+
+30 times do
+  Dog.create(
+    name: Faker::Creature::Dog.name
+    gender: Faker::Creature::Dog.gender
+    breed: Faker::Creature::Dog.breed
+    age: Faker::Creature::Dog.age
+    size: Faker::Creature::Dog.size
+    description: Faker::Lorem.paragraphs(number: 1)
+  )
+end
