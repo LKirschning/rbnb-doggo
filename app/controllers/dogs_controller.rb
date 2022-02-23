41503536f7c4bd502910bb11305292e883b358aa
Ludwig Kirschning <l.kirschning@yahoo.de>
2022-02-23 14:50:06 +0100

    disbaled skip_before_action

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,5 @@
 class DogsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index  ]
+  # skip_before_action :authenticate_user!, only: [ :index  ]
 
   def index
     @dogs = Dog.all
