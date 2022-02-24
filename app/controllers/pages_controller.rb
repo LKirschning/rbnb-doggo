57dff7b7e7110d2f30e6d3f1f7bea510345ba2d4
Ludwig Kirschning <l.kirschning@yahoo.de>
2022-02-24 12:42:35 +0100

    implemented dashboard, changed navbar, still need to fix new_dog image upload

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,15 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @user = current_user
+    @bookings = Booking.where(user_id:@user)
+
+    @dogs = Dog.where(user_id:@user)
+
+    # raise
+  end
+
+
 end
